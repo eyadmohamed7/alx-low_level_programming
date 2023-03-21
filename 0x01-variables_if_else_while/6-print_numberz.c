53323d93391de339cb1d79883ef95d4e3abbbe51
eyadmohamed7 <eyadmohamed348@gmail.com>
2023-03-20 19:46:45 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+        int digit = 0;
+
+        while (digit <= 9)
+        {
+                putchar(digit + '0');
+                digit++;
+        }
+        putchar("\n");
+
+        return (0);
+}
