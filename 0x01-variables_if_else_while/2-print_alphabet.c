6aaa9aa266866739480080915e8ebaf2bab81280
eyadmohamed7 <eyadmohamed348@gmail.com>
2023-03-20 17:34:04 -0700

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,8 +3,7 @@
 /**
  * main - entry point
  *
- * Description: print the value of n status:
- *              greater than, is zero and is not less than 6.
+ * Description: print all alphabet letters
  *
  * Return: 0 (success)
 */
